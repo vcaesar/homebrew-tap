ec3ac50bbcbf08d4cd883c18dc4b3367f2ad9a0c
vcaesar <vzvway@gmail.com>
2026-04-16 05:31:22 -0700

    Update: rm undefined method 'license'

--- Casks/vhand.rb
+++ Casks/vhand.rb
@@ -2,7 +2,6 @@
 cask "vhand" do
   desc "The AI translate and vocie assistant"
   homepage "https://github.com/vcaesar/vhand"
-  license "EULA"
   version "v0.10.2"
   arch arm: "arm64", intel: "amd64"
   sha256 arm: "402a5dfddbe338b5afb6e3b1ff3da4c2e14a5470547adc86a04c3ab2910a9297",
