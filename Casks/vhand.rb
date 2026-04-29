f8929714881d0a9d4e9688997a47aa0c09e850f9
vcaesar <vzvway@gmail.com>
2026-04-29 13:20:54 -0700

    Update: update version

--- Casks/vhand.rb
+++ Casks/vhand.rb
@@ -2,10 +2,10 @@
 cask "vhand" do
   desc "The AI translate and vocie assistant"
   homepage "https://github.com/vcaesar/vhand"
-  version "v0.10.2"
+  version "v0.20.0"
   arch arm: "arm64", intel: "amd64"
-  sha256 arm: "402a5dfddbe338b5afb6e3b1ff3da4c2e14a5470547adc86a04c3ab2910a9297",
-        intel: "402a5dfddbe338b5afb6e3b1ff3da4c2e14a5470547adc86a04c3ab2910a9297"
+  sha256 arm: "d1dadaa7fde6fcf2be09fa3d8d7ddac8a311a1c1f90b563c569326638daaec03",
+        intel: "d1dadaa7fde6fcf2be09fa3d8d7ddac8a311a1c1f90b563c569326638daaec03"
   url "https://github.com/vcaesar/vhand/releases/download/#{version}/Vhand.dmg",
   verified: "github.com/vcaesar/vhand"
   name "Vhand"
