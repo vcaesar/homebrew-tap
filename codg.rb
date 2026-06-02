e6a9afd7228a58853068c2f70310fc09c06f590d
vcaesar <vzvway@gmail.com>
2026-06-02 07:45:37 -0700

    Update: update version

--- codg.rb
+++ codg.rb
@@ -2,31 +2,31 @@
 class Codg < Formula
   desc "The next code and work AI agent"
   homepage "https://github.com/vcaesar/codg"
-  version "2.3.0"
+  version "2.3.1"
   license "EULA"
 
   on_macos do
     on_intel do
       url "https://github.com/vcaesar/codg/releases/download/v#{version}/codg_darwin_amd64.zip"
-      sha256 "8cf97b1a5040acc107ed7fdf9138b25c017e96c37d3e1124bd4687f6f2eb7d3c"
+      sha256 "9818aea6685cf6681c239f18863e45150a707d10f593b8ce608b132290c9cc13"
     end
 
      on_arm do
       url "https://github.com/vcaesar/codg/releases/download/v#{version}/codg_darwin_arm64.zip"
-      sha256 "95db094d2f1b7994939566c94c7c5e4e83ee33991f17fcc09f137dd55748cae2"
+      sha256 "70f8a75140bdfe0e01c393dc87cde2860d8127cab96c8a3ddeddb81c4470a869"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/vcaesar/codg/releases/download/v#{version}/codg_linux_amd64.zip"
-      sha256 "29a6500c201fe439f7808b1c18f9ab3c46faec94788785beab0726ee099bc8d4"
+      sha256 "881c07e4cf0be89f12911215fe95572051eaaa205a8a957fd641045223c4aebb"
     end
 
     on_arm do
       if Hardware::CPU.is_64_bit?
         url "https://github.com/vcaesar/codg/releases/download/v#{version}/codg_linux_arm64.zip"
-        sha256 "b06af74cea01f5d58477bd8effe75126b2d1d11273599f8195aef5fde3597741"
+        sha256 "41785e521b1b4951c4b6c090d7e271a64602bef7e3b970d93081381d27ddee8a"
       else
         url "https://github.com/vcaesar/codg/releases/download/v#{version}/codg_linux_armv7.zip"
         sha256 "REPLACE_WITH_SHA256_LINUX_ARMV7"
